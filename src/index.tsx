adeab92c5dae6ddc1654658bb0550e2f98855445
Shaghayegh Ghavami <ghavami.shgh@gmail.com>
2023-02-04 09:31:28 +0330

    Add material ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createRoot } from "react-dom/client";
+import CssBaseline from "@mui/material/CssBaseline";
+import App from "./App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <React.StrictMode>
+  <CssBaseline>
     <App />
-  </React.StrictMode>
+  </CssBaseline>
 );
-
