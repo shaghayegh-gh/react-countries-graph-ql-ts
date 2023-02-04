73cd626ca7a6f48e45d0641d949e0608b41210a6
Shaghayegh Ghavami <ghavami.shgh@gmail.com>
2023-02-04 09:39:00 +0330

    Add apollo client

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,18 @@
 import { createRoot } from "react-dom/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import CssBaseline from "@mui/material/CssBaseline";
 import App from "./App";
 
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_API_URL,
+  cache: new InMemoryCache(),
+});
+
 const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <CssBaseline>
-    <App />
-  </CssBaseline>
+  <ApolloProvider client={client}>
+    <CssBaseline>
+      <App />
+    </CssBaseline>
+  </ApolloProvider>
 );
